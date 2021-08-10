771c2e7824dbfbb84fe0e2030ab094cf673f561d
pocketech <hakuyowasedatech@gmail.com>
2021-08-10 11:06:55 +0900

    :recycle:50mb超過のためplaywrightからpuppeteerに変更

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -1,26 +1,25 @@
 // path: /pages/api/open-graph-image.ts
 import chromium from 'chrome-aws-lambda'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { chromium as playwrightChromium } from 'playwright-core'
+import puppeteer from 'puppeteer-core'
 
 // getAbsoluteURL is in a snippet further down
 import { getAbsoluteURL } from '@/utils/getAbsoluteURL'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Start the browser with the AWS Lambda wrapper (chrome-aws-lambda)
-  const browser = await playwrightChromium.launch({
+  const browser = await puppeteer.launch({
     args: chromium.args,
+    defaultViewport: {
+      width: 1200,
+      height: 630,
+    },
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
   })
   // Create a page with the Open Graph image size best practise
   // 1200x630 is a good size for most social media sites
-  const page = await browser.newPage({
-    viewport: {
-      width: 1200,
-      height: 630,
-    },
-  })
+  const page = await browser.newPage()
   // Generate the full URL out of the given path (GET parameter)
   const relativeUrl = (req.query['path'] as string) || ''
   const url = getAbsoluteURL(relativeUrl)
@@ -28,7 +27,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await page.goto(url, {
     timeout: 15 * 1000,
     // waitUntil option will make sure everything is loaded on the page
-    waitUntil: 'networkidle',
+    waitUntil: 'networkidle0',
   })
   const data = await page.screenshot({
     type: 'png',
