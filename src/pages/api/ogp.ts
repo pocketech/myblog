8639942c529654de1f4826054e119b506201f8ef
pocketech <hakuyowasedatech@gmail.com>
2021-08-10 20:18:06 +0900

    :bug:domcontentloadedに変更

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -27,7 +27,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await page.goto(url, {
     timeout: 15 * 1000,
     // waitUntil option will make sure everything is loaded on the page
-    waitUntil: 'networkidle0',
+    waitUntil: 'domcontentloaded',
   })
   const data = await page.screenshot({
     type: 'png',
