d30b2f6988b5bc2e43c43cfd4ef456da99ba1cb1
pocketech <hakuyowasedatech@gmail.com>
2021-08-17 02:15:09 +0900

    :fire:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,6 @@ import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import { config } from 'seo.config'
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-  require('../mocks')
-}
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const getLayout: (page: JSX.Element) => JSX.Element =
     (Component as any).getLayout || ((page) => page)
