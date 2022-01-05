2d3c583734390e51dd55c7b71786e0d43004f374
pocketech <hakuyowasedatech@gmail.com>
2022-01-05 21:35:24 +0900

    :sparkles:gaの追加

---
+++ src/libs/gtag.tsx
@@ -0,0 +1,93 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
+
+export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''
+
+// IDが取得できない場合を想定する
+export const isExistsGaId = GA_ID !== ''
+
+// PVを測定する
+export const pageview = (path: string) => {
+  window.gtag('config', GA_ID, {
+    page_path: path,
+  })
+}
+
+// GAイベントを発火させる
+export const event = ({ action, category, label, value = '' }: Event) => {
+  if (!isExistsGaId) {
+    return
+  }
+
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label ? JSON.stringify(label) : '',
+    value,
+  })
+}
+
+// _app.tsx で読み込む
+export const usePageView = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!isExistsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path: string) => {
+      pageview(path)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+}
+
+// _app.tsx で読み込む
+export const GoogleAnalytics = () => (
+  <>
+    {isExistsGaId && (
+      <>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <Script
+          id="gtag_init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());    
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
+      </>
+    )}
+  </>
+)
+
+// イベントを型で管理
+type ContactEvent = {
+  action: 'submit_form'
+  category: 'contact'
+}
+
+type ClickEvent = {
+  action: 'click'
+  category: 'other'
+}
+
+export type Event = (ContactEvent | ClickEvent) & {
+  label?: Record<string, string | number | boolean>
+  value?: string
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,16 @@ import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import { config } from 'seo.config'
 
+import { GoogleAnalytics, usePageView } from '@/libs/gtag'
+import { theme } from '@/theme'
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  usePageView()
   const getLayout: (page: JSX.Element) => JSX.Element =
     (Component as any).getLayout || ((page) => page)
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <GoogleAnalytics />
       <DefaultSeo {...config} />
       {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
