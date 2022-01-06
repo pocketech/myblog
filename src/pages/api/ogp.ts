d0fc36df73290574284cd7f011e8dd6f3985915d
pocketech <hakuyowasedatech@gmail.com>
2022-01-06 18:22:26 +0900

    Revert ":wrench:puppeteerからplaywrightへ変更"
    This reverts commit 422edf685fd482f827791847055bc5a664b11325.

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -1,27 +1,29 @@
+import chromium from 'chrome-aws-lambda'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import playwright from 'playwright-aws-lambda'
 
 import { OGP_IMAGE_SIZE } from '@/constants/ogpImageSize'
 import { getAbsoluteURL } from '@/utils/getAbsoluteURL'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await playwright.loadFont(
+  await chromium.font(
     'https://ghcdn.rawgit.org/googlefonts/noto-emoji/main/fonts/NotoColorEmoji.ttf'
   )
-  await playwright.loadFont(
+  await chromium.font(
     'https://ghcdn.rawgit.org/googlefonts/noto-cjk/main/Sans/SubsetOTF/JP/NotoSansJP-Medium.otf'
   )
   // Start the browser with the AWS Lambda wrapper (chrome-aws-lambda)
-  const browser = await playwright.launchChromium()
-  // Create a page with the Open Graph image size best practise
-  // 1200x630 is a good size for most social media sites
-
-  const page = await browser.newPage({
-    viewport: {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: {
       width: OGP_IMAGE_SIZE.width,
       height: OGP_IMAGE_SIZE.height,
     },
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
   })
+  // Create a page with the Open Graph image size best practise
+  // 1200x630 is a good size for most social media sites
+  const page = await browser.newPage()
   // Generate the full URL out of the given path (GET parameter)
   const relativeUrl = (req.query['path'] as string) || ''
   const url = getAbsoluteURL(relativeUrl)
@@ -29,7 +31,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await page.goto(url, {
     timeout: 15 * 1000,
     // waitUntil option will make sure everything is loaded on the page
-    waitUntil: 'networkidle',
+    waitUntil: 'networkidle0',
   })
   const data = await page.screenshot({
     type: 'png',
