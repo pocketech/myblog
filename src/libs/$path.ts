5d7e8f1b5722b9057c6b531fe9162b37cddeca58
Kenty <nakano-k@belong.co.jp>
2022-10-06 20:56:07 +0900

    :art:

--- src/libs/$path.ts
+++ src/libs/$path.ts
@@ -1,28 +1,48 @@
-/* eslint-disable */
-// prettier-ignore
 export const pagesPath = {
   $404: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/404" as const,
+      hash: url?.hash,
+    }),
   },
   about: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/about" as const,
+      hash: url?.hash,
+    }),
   },
   articles: {
     _id: (id: string | number) => ({
       ogp: {
-        $url: (url?: { hash?: string }) => ({ pathname: '/articles/[id]/ogp' as const, query: { id }, hash: url?.hash })
+        $url: (url?: { hash?: string }) => ({
+          pathname: "/articles/[id]/ogp" as const,
+          query: { id },
+          hash: url?.hash,
+        }),
       },
-      $url: (url?: { hash?: string }) => ({ pathname: '/articles/[id]' as const, query: { id }, hash: url?.hash })
-    })
+      $url: (url?: { hash?: string }) => ({
+        pathname: "/articles/[id]" as const,
+        query: { id },
+        hash: url?.hash,
+      }),
+    }),
   },
   contact: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/contact" as const,
+      hash: url?.hash,
+    }),
   },
   works: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/works' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/works" as const,
+      hash: url?.hash,
+    }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
-}
+  $url: (url?: { hash?: string }) => ({
+    pathname: "/" as const,
+    hash: url?.hash,
+  }),
+};
 
-// prettier-ignore
-export type PagesPath = typeof pagesPath
+export type PagesPath = typeof pagesPath;
